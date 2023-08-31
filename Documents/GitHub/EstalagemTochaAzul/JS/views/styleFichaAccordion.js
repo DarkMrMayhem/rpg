bb7154763a7cd0eceb705ebf772bda0f150ac225
wesley <wesleyabatista@unisc.br>
2023-08-31 09:47:41 -0300

    acordion arrumado

--- Documents/GitHub/EstalagemTochaAzul/JS/views/styleFichaAccordion.js
+++ Documents/GitHub/EstalagemTochaAzul/JS/views/styleFichaAccordion.js
@@ -1,24 +1,28 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
-event.preventDefault()
-
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+        event.preventDefault();
+        /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
         this.classList.toggle("active");
         
         /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
-        if (panel.style.display === "flex") {
+        if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
-            panel.style.display = "flex";
+            panel.style.display = "block";
         }
 
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+        if (panel.style.padding) {
+            panel.style.paddingTop = 15 + "px";
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
